8dd5c8175039857233681a37433cf895b99dc70b
Anton Kobelev <antonkobelev1985@gmail.com>
2019-02-14 17:17:14 +0500

    add a Rating

--- ActionListenerProject/app/src/main/java/com/example/actionlistenerproject/MainActivity.java
+++ ActionListenerProject/app/src/main/java/com/example/actionlistenerproject/MainActivity.java
@@ -1,13 +1,88 @@
 package com.example.actionlistenerproject;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText pass;
+    private Button btn,btn2;
+    private RatingBar rating;
+    private TextView text_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listenerOnClick();
     }
+
+    public void listenerOnClick (){
+        pass = (EditText)findViewById(R.id.editText);
+        btn = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(
+                                MainActivity.this,pass.getText(),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                }
+        );
+
+        rating = (RatingBar)findViewById(R.id.ratingBar);
+        text_show = (TextView)findViewById(R.id.textView);
+        rating.setOnRatingBarChangeListener(
+                new RatingBar.OnRatingBarChangeListener() {
+                    @Override
+                    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                        text_show.setText("Значение: " + String.valueOf(rating));
+                    }
+                }
+        );
+
+
+
+        btn2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+                        a_builder.setMessage("Вы хотите закрыть приложение?")
+                                .setCancelable(false)
+                                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+                                })
+                                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alert = a_builder.create();
+                        alert.setTitle("Закрытие приложения");
+                        alert.show();
+
+                    }
+                }
+        );
+
+
+
+    }
+
 }
